c547f916c369c7746ed4705f8c4ffeae343e43da
Compi1_201314778 <avilatorresics@gmail.com>
2020-08-20 15:59:04 -0600

    Updates

---
+++ precache-manifest.d3eb595eb46a00a74fd17b6a6e974b87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277f8d07b9817577e9789ad6cc3aa37d",
+    "url": "/matrioshtsdat/index.html"
+  },
+  {
+    "revision": "f45278ab89b0e87caf3e",
+    "url": "/matrioshtsdat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2c837f8d83f96b26719",
+    "url": "/matrioshtsdat/static/js/2.84b8bddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/matrioshtsdat/static/js/2.84b8bddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f45278ab89b0e87caf3e",
+    "url": "/matrioshtsdat/static/js/main.9837404b.chunk.js"
+  },
+  {
+    "revision": "de04655071fa26289dba",
+    "url": "/matrioshtsdat/static/js/runtime-main.4cc15dbf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/matrioshtsdat/static/media/logo.5d5d9eef.svg"
+  }
+]);
