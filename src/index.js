5aa1aa9682f0f930363351279ec8faa92e2eec8a
Compi1_201314778 <avilatorresics@gmail.com>
2020-08-20 19:08:47 -0600

    Test 2 ghp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+function retornaTexto (texto){
+  return texto + "\nahora está en area de texto de salida.";
+};
+
+class EssayForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Texto de entrada en area de texto '
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    //alert('An essay was submitted: ' + this.state.value);
+    document.getElementById('salida').value = retornaTexto(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <textarea value={this.state.value} onChange={this.handleChange} />
+        <div>
+        <input type="submit" value="Submit" />
+        </div>
+      </form>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <NameForm />,
+  document.getElementById('root1')
+);
+
+ReactDOM.render(
+  <EssayForm />,
+  document.getElementById('root2')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
