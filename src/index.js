e3f03511ff9f42256368cb17b390a408ff09a71e
Compi1_201314778 <avilatorresics@gmail.com>
2020-08-22 20:09:39 -0600

    Test 3 ghp tree table

--- src/index.js
+++ src/index.js
@@ -1,85 +1,149 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import retornaTexto from './ret';
-//import App from './App';
-//import * as serviceWorker from './serviceWorker';
+import Graph from 'vis-react';
+//import {retornaT1} from './ret';
+//import {retornaT2} from './ret';
+//import {retornaT2} from './asin/ret';
 
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
+let arr1 = ["mundo", "como", "estas"];
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
-
-class EssayForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 'Texto de entrada en area de texto '
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    //alert('An essay was submitted: ' + this.state.value);
-    document.getElementById('salida').value = retornaTexto(this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <textarea value={this.state.value} onChange={this.handleChange} />
-        <div>
-        <input type="submit" value="Submit" />
-        </div>
-      </form>
-    );
-  }
-}
-
-ReactDOM.render(
-  <NameForm />,
-  document.getElementById('root1')
+let row1 = (
+  <tr>
+    <td>{arr1[0]}</td>
+    <td>{arr1[1]}</td>
+    <td>{arr1[2]}</td>
+  </tr>
 );
 
-ReactDOM.render(
-  <EssayForm />,
-  document.getElementById('root2')
+/*
+for(let i=1;i<3;i++){
+  row1.push(
+    <tr>
+      <td>{arr1[0]}</td>
+      <td>{arr1[1]}</td>
+      <td>{arr1[2]}</td>
+    </tr>
+  );
+}*/
+let msj2 = [row1];
+msj2.push(row1);
+msj2.push(row1);
+msj2.push(row1);
+msj2.push(row1);
+let tabla1 = (
+  <div>
+    <table class="egt">
+      <tr>
+        <th>Hoy</th>
+        <th>Mañana</th>
+        <th>Martes</th>
+      </tr>
+      <tr>
+        <td>Soleado</td>
+        <td>Mayormente soleado</td>
+        <td>Parcialmente nublado</td>
+      </tr>
+      <tr>
+        <td>19°C</td>
+        <td>17°C</td>
+        <td>12°C</td>
+      </tr>
+      {msj2}
+    </table>
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+document.getElementById('btn1').onclick = function a() {
+  ReactDOM.render(
+    tabla1,
+    document.getElementById('root1')
+  );
+}
+
+/*
+document.getElementById('btn2').onclick = function b() {
+  ReactDOM.render(
+    <h1>Hola Universo</h1>,
+    document.getElementById('root1')
+  );
+}
+*/
+document.getElementById('btn2').onclick = function tree() {
+  var nodes = [];
+  var edges = [];
+  // randomly create some nodes and edges
+  for (var i = 0; i < 15; i++) {
+    nodes.push({ id: i, label: String(i) });
+  }
+  edges.push({ from: 0, to: 1 });
+  edges.push({ from: 0, to: 6 });
+  edges.push({ from: 0, to: 13 });
+  edges.push({ from: 0, to: 11 });
+  edges.push({ from: 1, to: 2 });
+  edges.push({ from: 2, to: 3 });
+  edges.push({ from: 2, to: 4 });
+  edges.push({ from: 3, to: 5 });
+  edges.push({ from: 1, to: 10 });
+  edges.push({ from: 1, to: 7 });
+  edges.push({ from: 2, to: 8 });
+  edges.push({ from: 2, to: 9 });
+  edges.push({ from: 3, to: 14 });
+  edges.push({ from: 1, to: 12 });
+  nodes[0]["level"] = 0;
+  nodes[1]["level"] = 1;
+  nodes[6]["level"] = 1;
+  nodes[11]["level"] = 1;
+  nodes[13]["level"] = 1;
+  nodes[7]["level"] = 2;
+  nodes[10]["level"] = 2;
+  nodes[12]["level"] = 2;
+  nodes[2]["level"] = 3;
+  nodes[3]["level"] = 4;
+  nodes[4]["level"] = 4;
+  nodes[8]["level"] = 4;
+  nodes[9]["level"] = 4;
+  nodes[5]["level"] = 5;
+  nodes[14]["level"] = 5;
+  // you can extend the options like a normal JSON variable:
+  var data = {
+    nodes: nodes,
+    edges: edges
+  };
+
+  var options = {
+    edges: {
+      smooth: {
+        type: 'cubicBezier',
+        forceDirection: 'vertical',
+        roundness: 0.4
+      },
+      color: 'black'
+    },
+    layout: {
+      hierarchical: {
+        direction: 'UD'
+      }
+    },
+    physics: false
+  };
+
+  var events = {
+    select: function (event) {
+      var { nodes, edges } = event;
+    }
+  };
+  
+  ReactDOM.render(
+    <Graph
+      graph={data}
+      options={options}
+      events={events}
+      getNetwork={this.getNetwork}
+      getEdges={this.getEdges}
+      getNodes={this.getNodes}
+      vis={vis => (this.vis = vis)}
+    />,
+    document.getElementById('mynetwork')
+  );
+}
